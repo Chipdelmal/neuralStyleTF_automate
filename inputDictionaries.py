21f0702fc32f4ef9f263abde4bcaed61a61f4b67
chipdelmal <chipdelmal@gmail.com>
2019-08-07 15:38:25 -0700

    Moved the dictionaries to another script

---
+++ inputDictionaries.py
@@ -0,0 +1,33 @@
+# Styles dictionary!
+imgEcuador = [
+    {"img": "ecuador00.jpg", "style": "g4.jpg", "iters": "30"},
+    {"img": "ecuador01.jpeg", "style": "g1.jpg", "iters": "75"},
+    {"img": "ecuador03.jpeg", "style": "g5.jpg", "iters": "75"},
+    {"img": "ecuador04.jpeg", "style": "g3.jpg", "iters": "200"},
+    {"img": "ecuador05.jpeg", "style": "flowers.png", "iters": "150"},
+    {"img": "ecuador06.jpeg", "style": "g6.jpg", "iters": "500"}
+]
+
+
+iterations = 500
+imgGremlin= [
+    {"img": "gremlin.jpg", "style": "t_2.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_3.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_5.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_6.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_8.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_9.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_10.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_11.jpg", "iters": str(iterations)},
+    {"img": "gremlin.jpg", "style": "t_13.jpg", "iters": str(iterations)}
+]
+
+
+iterations = 300
+styAbril = ["g4.jpg", "g1.jpg", "g5.jpg", "g3.jpg", "flowers.png", "g6.jpg"]
+imgAbril = [{
+        "img": "abril.jpg",
+        "iters": str(iterations),
+        "style": i
+    } for i in styAbril
+]
--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import subprocess
 import warnings
+import inputDictionaries as inDict
 warnings.filterwarnings("ignore")
 
+IMG_SIZE = 4000
 PATH = "/Users/sanchez.hmsc/Documents/GitHub/neuralStyleTF_automate/"
+imagesDict = inDict.imgEcuador
 PATH_OUT = PATH + "image_output/"
-IMG_SIZE = 1000
-
-# Styles dictionary!
-imagesDict = [
-    {"img": "quito.jpg", "style": "g4.jpg", "iters": "500"},
-    {"img": "ecuador01.jpeg", "style": "g1.jpg", "iters": "500"},
-    {"img": "ecuador03.jpeg", "style": "g5.jpg", "iters": "500"},
-    {"img": "ecuador04.jpeg", "style": "g3.jpg", "iters": "500"},
-    {"img": "ecuador05.jpeg", "style": "flowers.png", "iters": "500"},
-    {"img": "ecuador06.jpeg", "style": "g6.jpg", "iters": "500"}
-]
 
 # Automation part
 imgsN= len(imagesDict)
 for (i, params) in enumerate(imagesDict):
     # Create output folder
     (imgN, styN) = (params["img"].split(".")[0], params["style"].split(".")[0])
-    outFolder = PATH_OUT + imgN + "_" + styN
+    outFolder = PATH_OUT + "" + imgN + "-" + "" + styN
     subprocess.Popen(['mkdir', outFolder])
     # Generate bash command
     cmd = [
@@ -31,9 +23,9 @@ for (i, params) in enumerate(imagesDict):
         "--img_output_dir", outFolder,
         "--max_size", str(IMG_SIZE),
         "--max_iterations", params["iters"],
-        "--device", "/cpu:0",
-        "--verbose",
-        "--original_colors"
+        "--device", "/cpu:0"
+        # "--verbose",
+        # "--original_colors"
     ]
     # Print state
     alert = "* [Running image {}/{} ({})]"

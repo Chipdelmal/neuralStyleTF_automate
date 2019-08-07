46839fad504a163f8de312c9ca14244b5de79a32
chipdelmal <chipdelmal@gmail.com>
2019-08-07 10:49:01 -0700

    Minor style updates to the code

--- main.py
+++ main.py
@@ -4,22 +4,26 @@ warnings.filterwarnings("ignore")
 
 PATH = "/Users/sanchez.hmsc/Documents/GitHub/neuralStyleTF_automate/"
 PATH_OUT = PATH + "image_output/"
-IMG_SIZE = 500
+IMG_SIZE = 1000
 
-
+# Styles dictionary!
 imagesDict = [
-    {"img": "ecuador01.jpeg", "style": "g1.jpg", "iters": "50"},
-    {"img": "ecuador03.jpeg", "style": "g5.jpg", "iters": "50"}
+    {"img": "quito.jpg", "style": "g4.jpg", "iters": "500"},
+    {"img": "ecuador01.jpeg", "style": "g1.jpg", "iters": "500"},
+    {"img": "ecuador03.jpeg", "style": "g5.jpg", "iters": "500"},
+    {"img": "ecuador04.jpeg", "style": "g3.jpg", "iters": "500"},
+    {"img": "ecuador05.jpeg", "style": "flowers.png", "iters": "500"},
+    {"img": "ecuador06.jpeg", "style": "g6.jpg", "iters": "500"}
 ]
 
-
+# Automation part
 imgsN= len(imagesDict)
 for (i, params) in enumerate(imagesDict):
     # Create output folder
     (imgN, styN) = (params["img"].split(".")[0], params["style"].split(".")[0])
     outFolder = PATH_OUT + imgN + "_" + styN
     subprocess.Popen(['mkdir', outFolder])
-    # Run python script
+    # Generate bash command
     cmd = [
         "python", "neural_style.py",
         "--content_img", params["img"],
@@ -31,9 +35,11 @@ for (i, params) in enumerate(imagesDict):
         "--verbose",
         "--original_colors"
     ]
-    alert = "**************  [Running image {}/{} ({})] **************"
+    # Print state
+    alert = "* [Running image {}/{} ({})]"
     print("-" * 100)
     print(alert.format(str(i + 1), str(imgsN), params["img"]))
     print("-" * 100)
+    # Run command and wait until it finishes
     p = subprocess.Popen(cmd)
     p.wait()
